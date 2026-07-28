530b7a11a6a95d1a172b6c8bc1bad1890ce1af4b
Jack Kern <jack.w.kern@gmail.com>
2026-07-28 15:50:34 -0700

    Remove outdated worlds

--- projects/world_demo/worlds/StressForest500k.js
+++
@@ -1,7 +0,0 @@
-class StressForest500k extends World {
-  static roots = [{
-    module: "StressForest500k",
-    transform: [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-    expand: true,
-  }];
-}
